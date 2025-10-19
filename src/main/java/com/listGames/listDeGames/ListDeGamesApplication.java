475a89cc1f497af68647ab47a67bd7e746f05eac
N78braga <nandobraga78@gmail.com>
2025-10-19 16:24:09 -0300

    Project created

---
+++ src/main/java/com/listGames/listDeGames/ListDeGamesApplication.java
@@ -0,0 +1,13 @@
+package com.listGames.listDeGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListDeGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListDeGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/listGames/listDeGames/ListDeGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listGames.listDeGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListDeGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
